6d93ac09793e03d1abb98b22b7a2439e5ce4ef00
kethagani Jyothsna <jyothsnak2309@gmail.com>
2026-07-27 20:44:09 +0530

    Create finding a quadratic equation.cpp

---
+++ finding a quadratic equation.cpp
@@ -0,0 +1,19 @@
+//Using cpp finding a quadratic equation
+#include<iostream>
+#include<cmath>
+int main()
+{
+	float a,b,c;
+	cout<<"Enter a,b and c coefficients:"<<end1;
+	cin>>a>>b>>c;
+	float d=b*b-4*a*c;
+	float root1,root2;
+	if(a==0)
+	{
+		cout<<"This is not quadratic equation"<<end1:
+	}
+	else if(d>0)
+	{
+		
+	}
+}
