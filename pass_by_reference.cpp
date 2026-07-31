b6301da6e131df1d5efa13192b85717e18003a8a
kethagani Jyothsna <jyothsnak2309@gmail.com>
2026-07-31 21:23:24 +0530

    Create pass_by_reference.cpp

---
+++ pass_by_reference.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+class Book
+{
+	public:
+		int price;
+		void getdata()
+		{
+			cout<<"Enter Original Book Price";
+			cin>>price;
+		}
+};
+void updateprice(Book &b)//pass by reference
+{
+	b.price=500;
+	cout<<"inside function book price="<<b.price;
+}
+int main()
+{
+	Book b1;
+	b1.getdata();
+	updateprice(b1);
+	cout<<"Outside function Actual book price="<<b1.price;
+	return 0;
+}
